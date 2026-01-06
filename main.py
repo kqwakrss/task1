1cc37894392cce9fce9efa1cbb49a373f50c88f1
Max <kqwakrss@gmail.com>
2026-01-06 14:44:32 +0200

    task_4 add pages/{slug}/title

--- main.py
+++ main.py
@@ -12,13 +12,6 @@ PAGES = {
     "about": {"title":"abobut us"}
 }
 
-@app.get("/pages/{slug}")
-def pages_slug(slug: str):
-    page = PAGES.get(slug)
-    if not page:
-        raise HTTPException (status_code = 404, detail = "Not found")
-    return page
-
 @app.get("/pages")
 def pages():
     result = []
@@ -27,3 +20,18 @@ def pages():
         ({"slug": slug,
          "title": PAGES[slug]["title"]})
     return result
+
+@app.get("/pages/{slug}")
+def pages_slug(slug: str):
+    page = PAGES.get(slug)
+    if not page:
+        raise HTTPException (status_code = 404, detail = "Not found")
+    return page
+
+@app.get("/pages/{slug}/title")
+def pages_slug_title(slug: str):
+    page_title = PAGES.get(slug)
+    if not page_title:
+        raise HTTPException (status_code = 404, detail = "Not found")
+    title = page_title.get("title")
+    return {"title": title}
